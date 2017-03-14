74d142eb58d4f438d0b6c3032924b7f70673b419
Dayne Broderson <broderson@gmail.com>
2017-03-13 22:11:26 -0800

    updating to 5.1.16 for win&osx users
    not tested yet in a good way

--- attributes/default.rb
+++ attributes/default.rb
@@ -23,9 +23,9 @@ default['virtualbox']['version'] = ''
 
 case node['platform_family']
 when 'mac_os_x'
-  default['virtualbox']['url'] = 'http://download.virtualbox.org/virtualbox/5.1.14/VirtualBox-5.1.14-112924-OSX.dmg'
+  default['virtualbox']['url'] = 'http://download.virtualbox.org/virtualbox/5.1.16/VirtualBox-5.1.16-113841-OSX.dmg'
 when 'windows'
-  default['virtualbox']['url'] = 'http://download.virtualbox.org/virtualbox/5.1.14/VirtualBox-5.1.14-112924-Win.exe'
+  default['virtualbox']['url'] = 'http://download.virtualbox.org/virtualbox/5.1.16/VirtualBox-5.1.16-113841-Win.exe'
   default['virtualbox']['version'] = Vbox::Helpers.vbox_version(node['virtualbox']['url'])
 when 'debian', 'rhel', 'fedora'
   default['virtualbox']['version'] = '5.1'
